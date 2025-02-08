1d1ab24f79f460570849831008b0e3fcc285ece1
vincent-softdev <vinh.ly.softdev@gmail.com>
2025-02-09 00:14:08 +1030

    initial PHP Laravel

---
+++ week1/database/factories/InvoiceFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Customer;
+use App\Models\Invoice;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invoice>
+ */
+class InvoiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Invoice::class;
+
+    public function definition(): array
+    {
+        $status = $this->faker->randomElement('B', 'P', 'V');
+        
+        return [
+            'customer_id' => Customer::factory(),
+            'amount' => $this->faker->numberBetween(100, 20000),
+            'status' => $status,
+            'billed_date' => $this->faker->dateTimeThisDecade(),
+        ];
+    }
+}
